3e2a4e26b1d7bbc334ecec39a0c4f3dcb21cfdb7
Aaron Siegel <datadreamerlabs@gmail.com>
2017-08-09 22:15:32 -0700

    rect and ellipse drawing with constrain proportions.

--- idsn520/shapes/sketch.js
+++ idsn520/shapes/sketch.js
@@ -2,40 +2,155 @@ var gradient;
 var menu;
 var drawImg;
 var dotSize = 10;
+var drawing = false;
+var constrain = false;
+var drawMode = 0; // 0 = rect, 1 = ellipse, 2 = polygon
+var createMode;
+var startX, startY;
+var endX, endY;
+var d = 0;  // diameter used for drawing
+var canvas;
 
 function preload(){
   gradient = loadImage("../designatusc_gradient.png");
 }
 
 function setup(){
-  var canvas = createCanvas(windowWidth, windowHeight);
+  canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("sketch");
+  //context = canvas.getContext('2D');
   frameRate(60);
-  rectMode(CENTER);
-  ellipseMode(CENTER);
+  createMode = CORNER;
+  rectMode(createMode);
+  ellipseMode(createMode);
   textAlign(CENTER, CENTER);
   noStroke();
+  constrain = false;
   drawImg = createGraphics(width, height);
   drawImg.background(255);
+  drawImg.fill(0);
+  drawImg.noStroke();
+  drawImg.rectMode(createMode);
+  drawImg.ellipseMode(createMode);
   menu = new Menu(gradient);
 }
 
 function draw(){
   background(255);
   image(drawImg, 0, 0, width, height);
-
+  if(drawing){
+    rectMode(createMode);
+    ellipseMode(createMode);
+    stroke(255,0,0);
+    fill(0,0,0,50);
+    drawShape(this);
+  }
   menu.draw();
 }
 
-function keyPressed(){
-  if(keyCode == 187){
-    dotSize++;
-  } else if(keyCode == 189){
-    if(dotSize > 1){
-      dotSize--;
+function drawShape(c){
+  endX = mouseX;
+  endY = mouseY;
+  var w = endX-startX;
+  var h = endY-startY;
+  if(createMode == CENTER){
+    w *= 2;
+    h *= 2;
+  }
+  if(drawMode == 0){              // rectangle
+    drawRectangle(c, w, h);
+  } else if(drawMode == 1){       // ellipse
+    drawEllipse(c, w, h);
+  } else if(drawMode == 2){       // polygon
+
+  }
+}
+
+function drawEllipse(c, w, h){
+  if(createMode == CORNER){
+    if(constrain){
+      d = Math.sqrt(w*w + h*h);
+      if(w < 0 && h >= 0){
+        c.ellipse(startX - d, startY, d, d);
+      } else if(w < 0 && h < 0){
+        c.ellipse(startX - d, startY - d, d, d);
+      } else if(w > 0 && h <= 0){
+        c.ellipse(startX, startY - d, d, d);
+      } else {
+        c.ellipse(startX, startY, d, d);
+      }
+    } else {
+      if(w < 0 && h >= 0){
+        c.ellipse(startX + w, startY, w, h);
+      } else if(w < 0 && h < 0){
+        c.ellipse(startX + w, startY + h, w, h);
+      } else if(w > 0 && h <= 0){
+        c.ellipse(startX, startY + h, w, h);
+      } else {
+        c.ellipse(startX, startY, w, h);
+      }
     }
-  } else if(keyCode == ENTER || keyCode == RETURN){
+  } else {
+    if(constrain){
+      d = Math.sqrt(w*w + h*h);
+      c.ellipse(startX, startY, d, d);
+    } else {
+      c.ellipse(startX, startY, w, h);
+    }
+  }
+}
+
+function drawRectangle(c, w, h){
+  if(createMode == CORNER){
+    if(constrain){
+      d = Math.sqrt(w*w + h*h);
+      if(w < 0 && h >= 0){
+        c.rect(startX, startY, -d, d);
+      } else if(w < 0 && h < 0){
+        c.rect(startX, startY, -d, -d);
+      } else if(w > 0 && h <= 0){
+        c.rect(startX, startY, d, -d);
+      } else {
+        c.rect(startX, startY, d, d);
+      }
+    } else {
+      c.rect(startX, startY, w, h);
+    }
+  } else {
+    if(constrain){
+      d = Math.sqrt(w*w + h*h);
+      c.rect(startX, startY, d, d);
+    } else {
+      c.rect(startX, startY, w, h);
+    }
+  }
+}
+
+function keyPressed(){
+  if(keyCode == ENTER || keyCode == RETURN){
     saveImage();
+  } else if(keyCode == SHIFT){
+      constrain = true;
+  } else if(key == "1") {
+    drawMode = 0; // rect
+  } else if(key == "2") {
+    drawMode = 1; // ellipse
+  } else if(key == "3") {
+    drawMode = 2; // polygon
+  } else if(key == "0"){
+    if(createMode == CORNER){
+      createMode = CENTER;
+    } else {
+      createMode = CORNER;
+    }
+    drawImg.rectMode(createMode);
+    drawImg.ellipseMode(createMode);
+  }
+}
+
+function keyReleased(){
+  if(keyCode == SHIFT){
+    constrain = false;
   }
 }
 
@@ -43,13 +158,18 @@ function mousePressed(){
   if(menu.isOver()){
     menu.mousePressed();
   } else {
-    // TODO: draw some shapes
+    drawing = true;
+    startX = mouseX;
+    startY = mouseY;
   }
 }
 
 function mouseReleased(){
   if(menu.isOver()){
     menu.mouseReleased();
+  } else {
+    drawShape(drawImg);
+    drawing = false;
   }
 }
 
@@ -67,4 +187,8 @@ function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
   drawImg = createGraphics(width, height);
   drawImg.background(255);
+  drawImg.fill(0);
+  drawImg.noStroke();
+  drawImg.rectMode(createMode);
+  drawImg.ellipseMode(createMode);
 }
