ff9b81e31e35188a3ce04d47adcc911cece06bad
Aaron Siegel <datadreamerlabs@gmail.com>
2017-12-03 16:05:38 -0800

    draw lines between touches

--- minis/userinterface/touch/sketch.js
+++ minis/userinterface/touch/sketch.js
@@ -1,6 +1,5 @@
 var circles = [];
 var circle;
-var rectangles = [];
 var sparkles = [];
 var pressTime;
 var clickDelay = 200;
@@ -26,23 +25,18 @@ function setup(){
 function draw(){
   colorMode(HSB);
   background(0);
-  fill(255);
+  stroke(255);
+  strokeWeight(5);
 
+  for(var i=1; i<touches.length; i++){
+    line(touches[i].x, touches[i].y, touches[i-1].x, touches[i-1].y);
+  }
   for(var i=0; i<touches.length; i++){
-    print(touches[i].id);
+    fill(colors[i]);
     ellipse(touches[i].x, touches[i].y, 100, 100);
   }
 
   noStroke();
-  // for(var i=0; i<rectangles.length; i++){
-  //   rectangles[i].draw();
-  // }
-  // for(var i=rectangles.length-1; i>=0; i--){
-  //   if(rectangles[i].done){
-  //     rectangles.splice(i,1);
-  //   }
-  // }
-  //
   // for(var i=circles.length-1; i>=0; i--){
   //   circles[i].draw();
   //   if(circles[i].dead){
@@ -176,55 +170,5 @@ Sparkle.prototype = {
       line(10 + p * 50,0,p * 100,0);
     }
     pop();
-  }
-}
-
-
-
-
-function Rectangle(down){
-  this.down = down;
-  this.done = false;
-  this.expanding = true;
-  this.fading = false;
-  this.startTime = millis();
-  this.duration = 500;
-  this.w = width;
-  this.h = 0;
-  this.c = random(360);
-  this.a = 1;
-}
-
-Rectangle.prototype = {
-  constructor:Rectangle,
-
-  draw:function(){
-    if(this.expanding){
-      var p = (millis() - this.startTime) / this.duration;
-      if(p >= 1){
-        this.expanding = false;
-        this.fading = true;
-        this.h = height;
-      } else {
-        this.h = height * this.sinProgress(p);
-      }
-    }
-    if(this.fading){
-      this.a -= 0.01;
-      if(this.a <= 0){
-        this.fading = false;
-        this.done = true;
-      }
-    }
-    fill(this.c, 255, 255, this.a);
-    if(this.down){
-      rect(0, 0, this.w, this.h);
-    } else {
-      rect(0, height-this.h, this.w, this.h);
-    }
-  },
-
-  sinProgress:function(p){
-    return (1 - (cos(PI * p) / 2 - 0.5)) - 1;
   }
 }
