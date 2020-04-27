e1fd5fce290eb6279ad71f081baba9e4de3b95ab
Aaron Siegel <datadreamerlabs@gmail.com>
2020-04-26 19:50:17 -0700

    removed unused confusing code.

--- minis/userinterface/touch/sketch.js
+++ minis/userinterface/touch/sketch.js
@@ -1,17 +1,10 @@
-var circles = [];
-var circle;
-var sparkles = [];
 var colors = [];
-
-
-
 
 function setup(){
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("sketch");
   frameRate(60);
 
-  var size = sqrt(sq(width) + sq(height));
   colors.push(color(255,0,0));
   colors.push(color(255,255,0));
   colors.push(color(0,255,0));
@@ -38,114 +31,8 @@ function draw(){
     fill(colors[c]);
     ellipse(touches[i].x, touches[i].y, 100, 100);
   }
-
-  noStroke();
-  // for(var i=circles.length-1; i>=0; i--){
-  //   circles[i].draw();
-  //   if(circles[i].dead){
-  //     circles.splice(i,1);
-  //   }
-  // }
-  //
-  for(var i=sparkles.length-1; i>=0; i--){
-    sparkles[i].draw();
-    if(sparkles[i].dead){
-      sparkles.splice(i,1);
-    }
-  }
-}
-
-
-
-
-function touchStarted(){
-
-}
-
-function touchEnded(){
-  sparkles.push(new Sparkle(mouseX, mouseY));
-}
-
-function touchMoved(){
-
 }
 
 function windowResized(){
   resizeCanvas(windowWidth, windowHeight);
-}
-
-
-
-
-
-
-function Circle(x, y){
-  this.x = x;
-  this.y = y;
-  this.d = 0;
-  this.td = 100;
-  this.pd = 0;
-  this.expanding = true;
-  this.contracting = false;
-  this.dead = false;
-  this.h = random(360);
-}
-
-Circle.prototype = {
-  constructor:Circle,
-
-  draw:function(){
-    if(this.expanding){
-      this.d += 5;
-    } else if(this.contracting){
-      this.d -= 5;
-      if(this.d <= 0){
-        this.contracting = false;
-        this.d = 0;
-        this.dead = true;
-      }
-    }
-    noFill();
-    stroke(this.h, 0, 255);
-    strokeWeight(10);
-    ellipse(this.x, this.y, this.d, this.d);
-  },
-
-  sinProgress:function(p){
-    return (1 - (cos(PI * p) / 2 - 0.5)) - 1;
-  }
-}
-
-
-
-
-function Sparkle(x, y){
-  this.x = x;
-  this.y = y;
-  this.lifespan = 1000;
-  this.birth = millis();
-  this.dead = false;
-  this.segments = random(10,20);
-  this.angle = TWO_PI / this.segments;
-}
-
-Sparkle.prototype = {
-  constructor:Sparkle,
-
-  draw:function(){
-    var p = (millis() - this.birth) / this.lifespan;
-    if(p >= 1){
-      this.dead = true;
-    }
-    strokeWeight(5 * (1-p));
-    stroke(255, 1-p);
-    push();
-    translate(this.x, this.y);
-    rotate(p);
-    for(var i=0; i<this.segments; i++){
-      rotate(this.angle);
-      line(10 + p * 50,0,p * 100,0);
-    }
-    pop();
-  }
 }
