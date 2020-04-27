195351d077bfe72308dc762f7eccfc1188ce3f0d
Aaron Siegel <datadreamerlabs@gmail.com>
2020-04-26 18:29:37 -0700

    Acceleration for background colors.

--- minis/userinterface/accel/sketch.js
+++ minis/userinterface/accel/sketch.js
@@ -2,13 +2,15 @@ function setup(){
   var canvas = createCanvas(windowWidth, windowHeight);
   canvas.parent("sketch");
   frameRate(60);
+  fill(255);
+  textSize(32);
 }
 
 function draw(){
-  background(255);
-  background(220, 50);
-  fill('magenta');
-  ellipse(width / 2, height / 2, accelerationZ);
+  background(accelerationX, accelerationY, accelerationZ);
+  text("x: " + accelerationX, 50, 50);
+  text("y: " + accelerationY, 50, 80);
+  text("z: " + accelerationZ, 50, 110);
 }
 
 function windowResized(){
